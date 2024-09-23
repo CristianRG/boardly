30c3d73dd3c267d3a46cdf67ed20eab0d0a46ca2
CristianRG <cristianrizogomez890@gmail.com>
2024-09-23 08:39:28 -0600

    updated handleRoutes to handle logged routes when users is authenticated

--- src/helpers/handleRoutes.js
+++ src/helpers/handleRoutes.js
@@ -1,8 +1,12 @@
 import store from "../store/store"
 import { handleLocalStorage } from "./handleLocalStorage"
-const {handleGetBoards} = handleLocalStorage()
+const { handleGetBoards } = handleLocalStorage()
+import { handleFetch } from "./handleFetch";
+import User from "../models/User";
+import Board from "../models/Board";
+const { handleAuth, handleFetchBoards } = handleFetch()
 
-const { BOARDS, BOARD, HOME, LOGIN } = { BOARDS: 'Boards', BOARD: 'Board', HOME: 'Home', LOGIN: 'Login'};
+const { BOARDS, BOARD, HOME, LOGIN } = { BOARDS: 'Boards', BOARD: 'Board', HOME: 'Home', LOGIN: 'Login' };
 
 export const handleRoutes = () => {
 
@@ -33,27 +37,75 @@ export const handleRoutes = () => {
      * 
      * @param {import("vue-router").RouteLocationNormalized} to 
      * @param {import("vue-router").RouteLocationNormalized} from 
-     * @param {import("vue-router").NavigationGuardNext} next 
+     * @param {import("vue-router").NavigationGuardNext} next
      */
-    const beforeEachLogged = (to, from, next) => {
-        if ((to.name !== HOME && to.name !== LOGIN) && !store.user) {
-            next({ name: LOGIN })
-        }
-        else if (to.name === BOARDS || to.name === BOARD && (store.boards.length <= 0 || !store.boards) && store.user) {
+    const beforeEachLogged = async (to, from, next) => {
+        if (to.meta.requireAuth && !store.user && to.name !== LOGIN) {
             try {
-                const boards = handleGetBoards();
-                store.boards = boards
+                const response = await handleAuth()
+                if (response.status == 200) {
+                    const result = await response.json()
+                    store.user = User.fromJSON(result.publicUser)
+                    next()
+                }
+                else {
+                    next({ name: LOGIN })
+                }
             } catch (error) {
-                store.boards = []
+                next({ name: LOGIN })
             }
-            next()
+        }
+        else if (to.name === LOGIN && !store.user) {
+            try {
+                const response = await handleAuth()
+                if (response.status == 200) {
+                    const result = await response.json()
+                    store.user = User.fromJSON(result.publicUser)
+                    next({ name: BOARDS })
+                }
+                else {
+                    next()
+                }
+            } catch (error) {
+                next()
+            }
+        }
+        // execute when is logged (has a cookie called token), user is not defined and not is the main route
+        if ((to.name !== HOME && to.name !== LOGIN) && !store.user) {
+            try {
+                const response = await handleAuth()
+                if (response.status == 200) {
+                    const result = await response.json()
+                    store.user = User.fromJSON(result.publicUser)
+                    next()
+                }
+                else {
+                    next({ name: LOGIN })
+                }
+            } catch (error) {
+                next({ name: LOGIN })
+            }
+        }
+       
+        else if (to.name == BOARDS && store.user) {
+            try {
+                const response = await handleFetchBoards(store.user)
+                if (response.status == 200) {
+                    let result = await response.json()
+                    store.boards = result.map((board) => { return Board.fromJSON(board) })
+                    next()
+                }
+            } catch (error) {
+
+            }
         }
         else {
             next()
         }
     }
-    
+
     return {
-        beforeEachNotLogged
+        beforeEachNotLogged,
+        beforeEachLogged
     }
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,13 +10,13 @@ import Notification from '../models/NotificationModel'
 import { uuid } from 'vue-uuid'
 
 const {handleGetBoards} = handleLocalStorage()
-const {beforeEachNotLogged} = handleRoutes()
+const {beforeEachNotLogged, beforeEachLogged} = handleRoutes()
 
 const routes = [
   { path: '/', name: 'Home', component: AppHome },
   { path: '/login', name: 'Login', component: () => import('../views/BoardlyLogin.vue') },
-  { path: '/boardly', name: 'Boards', component: () => import('../views/BoardlyHome.vue') },
-  { path: '/boardly/:id', name: 'Board', props: true, component: () => import('../views/BoardlyBoard.vue') },
+  { path: '/boardly', name: 'Boards', meta: {requireAuth: true}, component: () => import('../views/BoardlyHome.vue') },
+  { path: '/boardly/:id', name: 'Board', meta: {requireAuth: true}, props: true, component: () => import('../views/BoardlyBoard.vue') },
   { path: '/share/:id', name: 'Share', props: true, component: () => import('../views/BoardlyShareBoard.vue')},
   { path: '/:pathMatch(.*)*', name: '404', component: () => import('../views/Error404.vue') }
 ]
@@ -27,7 +27,12 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  beforeEachNotLogged(to, from, next)
+  if(!store.logged){
+    beforeEachNotLogged(to, from, next)
+  }
+  else {
+    beforeEachLogged(to, from, next)
+  }
 })
 
 router.afterEach((to, from, failure) => {
