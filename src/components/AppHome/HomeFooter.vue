6b6299a346b16bcce9d1309a05c86d59941bbbc7
CristianRG <cristianrizogomez890@gmail.com>
2024-09-23 10:55:56 -0600

    new design in main page added

---
+++ src/components/AppHome/HomeFooter.vue
@@ -0,0 +1,27 @@
+<template>
+    <footer>
+        <div class="icon">
+            <GitHub />
+        </div>
+    </footer>
+</template>
+<script setup>
+import GitHub from '../icons/GitHub.vue';
+</script>
+<style scoped>
+footer {
+    width: 100%;
+    height: 4rem;
+    display: flex;
+    align-items: center;
+    padding: 12px;
+}
+
+.icon {
+    transition: var(--tran-03);
+}
+
+.icon:hover {
+    transform: scale(1.1);
+}
+</style>
--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -1,10 +1,25 @@
 <template>
     <HomeHeader />
-    <h1>Welcome to Boardly App</h1>
-    <h2>Boardly is a virtual board application</h2>
-    <button class="btn btn-success" @click="getBoards">Get started</button>
+    <main>
+        <section style="margin-left: 12px;">
+            <h1 style="font-size: 40px; margin: 0%;">Boarldy Note App</h1>
+            <p style="font-size: 20px;">Una aplicación de notas minimalista.
+                <br>Organiza tus actividades en flujos de trabajo.
+                <br><strong>Simple, sencilla y eficiente.</strong>
+                <br>
+                <br>¡Comienza ahora!
+            </p>
+            <button class="btn btn-success" @click="getBoards">Comenzar</button>
+        </section>
+        <section>
+            <img v-if="store.theme == 'dark'" src="/public/img/dark_design_boardly.webp" alt="dark_design">
+            <img v-if="store.theme == 'light'" src="/public/img/light_design_boardly.webp" alt="ligth_design">
+            <span style="display: inline-block; padding-left: 10px; font-size: 20px; font-style: italic;">Take notes, organize your thoughts</span>
+        </section>
+    </main>
+    <HomeFooter />
     <Teleport to="body">
-        <AlertTemplate v-if="showAlert" :alert @close="showAlert = false"/>
+        <AlertTemplate v-if="showAlert" :alert @close="showAlert = false" />
     </Teleport>
 </template>
 
@@ -13,20 +28,21 @@
 import { ref } from 'vue';
 import HomeHeader from '../components/AppHome/HomeHeader.vue';
 import AlertTemplate from '../components/Alerts/AlertTemplate.vue';
+import HomeFooter from '../components/AppHome/HomeFooter.vue';
 import { useUser } from '../composables/useUser';
 import Alert from '../models/Alert';
 import store from '../store/store';
 import router from '../routes/routes';
 
 const showAlert = ref(false)
-const {setUser, saveUserState} = useUser()
+const { setUser, saveUserState } = useUser()
 
 const getBoards = () => {
-    if(!store.user){
+    if (!store.user) {
         showAlert.value = true;
     }
-    else{
-        router.push({name: 'Boards'})
+    else {
+        router.push({ name: 'Boards' })
     }
 }
 
@@ -43,16 +59,29 @@ alert.message = `Por el momento no podemos hacer un registro de sus datos. Sin e
                 sus tableros serán almacenados de forma local en su navegador.`
 alert.actions = [
     Alert.action('Cancelar', alert.styles.btnDanger, () => showAlert.value = false),
-    Alert.action('Aceptar', alert.styles.btnSuccess, () => {continueAsGuess(), showAlert.value = false})
+    Alert.action('Aceptar', alert.styles.btnSuccess, () => { continueAsGuess(), showAlert.value = false })
 ]
 
 </script>
 <style scoped>
-.button{
-    background: #623EE6;
-    color: white;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    padding: 10px 20px;
+main {
+    width: 100%;
+    height: calc(100% - 8rem);
+    margin-top: 4rem;
+    display: flex;
+    color: var(--text-color);
+
+    & section {
+        width: 50%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+
+        & button {
+            width: fit-content;
+        }
+    }
 }
+
 </style>
